88147a8a2add5f3d17b9d2ff8dd1e758a1953305
FierceSpectrum <127576883+FierceSpectrum@users.noreply.github.com>
2024-04-03 23:38:17 -0600

    error letras quitado

--- Calculadora.py
+++ Calculadora.py
@@ -5,35 +5,37 @@ import Multiplicacion
 import Resta
 import Suma
 
+try:
+    mensaje = "Ingrese un numero: "
+    numero1 = int(input(mensaje))
 
-mensaje = "Ingrese un numero: "
-numero1 = int(input(mensaje))
-
-mensaje = """Ingrese una opcion:
-1 -> Sumar
-2 -> Restar
-3 -> Multiplicar
-4 -> Dividir
-
-
-..."""
-operador = int(input(mensaje))
-
-mensaje = "Ingrese otro numero: "
-numero2 = int(input(mensaje))
+    mensaje = """Ingrese una opcion:
+    1 -> Sumar
+    2 -> Restar
+    3 -> Multiplicar
+    4 -> Dividir
 
 
-if operador == 1:
-    mensaje = f"EL resultado de la suma es {Suma.sumar(numero1, numero2)}"
-elif operador == 2:
-    mensaje = f"EL resultado de la resta es {Resta.restar(numero1, numero2)}"
+    ..."""
+    operador = int(input(mensaje))
 
-elif operador == 3:
-    mensaje = f"EL resultado de la multiplicacion es {Multiplicacion.multiplicar(numero1, numero2)}"
+    mensaje = "Ingrese otro numero: "
+    numero2 = int(input(mensaje))
 
-elif operador == 4:
-    mensaje = f"EL resultado de la divicion es {Divicion.dividir(numero1, numero2)}"
 
-else:
-    mensaje = "Deves de ingresar una opcion valida"
-print(mensaje)
+    if operador == 1:
+        mensaje = f"EL resultado de la suma es {Suma.sumar(numero1, numero2)}"
+    elif operador == 2:
+        mensaje = f"EL resultado de la resta es {Resta.restar(numero1, numero2)}"
+
+    elif operador == 3:
+        mensaje = f"EL resultado de la multiplicacion es {Multiplicacion.multiplicar(numero1, numero2)}"
+
+    elif operador == 4:
+        mensaje = f"EL resultado de la divicion es {Divicion.dividir(numero1, numero2)}"
+
+    else:
+        mensaje = "Deves de ingresar una opcion valida"
+    print(mensaje)
+except:
+    print("deves de ingresar numeros no letras.")
